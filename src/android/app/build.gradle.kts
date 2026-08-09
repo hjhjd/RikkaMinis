a3f00c89d4ba710ae789545749c3c0dd76442b02
hjhjd <1276516805@qq.com>
2026-08-10 05:29:28 +0800

    将 Android 应用更名为 VCPMinis 并使用独立包名

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.openminis.app"
+        applicationId = "com.vcp.rikkaminis"
         minSdk = 26
         targetSdk = 35
         versionCode = ciVersionCode ?: 22
